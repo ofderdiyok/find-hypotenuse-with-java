21199880fc8a9691174631a2226e52fbb9214252
Faruk Derdiyok <farukderdiyok01@gmail.com>
2021-02-16 16:46:28 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String isim;
+    
+        Scanner scan = new Scanner(System.in);
+        System.out.println("İsminizi giriniz.");
+        isim = scan.nextLine();
+        for (int i = isim.length()-1; i >= 0 ; i--) {
+            System.out.print(isim.charAt(i));
+        }
+        System.out.println("");
+    }
+}
